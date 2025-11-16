f8e906165f14975ac1f3706a40a7bc0f1fe83c3d
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2025-11-16 12:32:34 -0600

    Update Program.cs

--- ReserveBlockCore/Program.cs
+++ ReserveBlockCore/Program.cs
@@ -81,7 +81,7 @@ namespace ReserveBlockCore
                 });
             }
             //Forced Testnet
-            //Globals.IsTestNet = true;
+            Globals.IsTestNet = true;
             Globals.V4Height = Globals.IsTestNet ? 1 : 3_074_181;//change for mainnet.
             Globals.V2ValHeight = Globals.IsTestNet ? 0 : 3_074_180;//change for mainnet.
             Globals.SpecialBlockHeight = Globals.IsTestNet ? 2000 : 3_074_185;//change for mainnet.
