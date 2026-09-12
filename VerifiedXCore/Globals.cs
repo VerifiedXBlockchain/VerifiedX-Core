812fec5568ad24a16eaa5215e10a9c2335359e89
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2026-09-12 15:34:04 -0500

    version dump

--- VerifiedXCore/Globals.cs
+++ VerifiedXCore/Globals.cs
@@ -230,7 +230,7 @@ namespace VerifiedXCore
         public static int APIPortSSL = 7777;
         public static int MajorVer = 7;
         public static int MinorVer = 0;
-        public static int RevisionVer = 0;
+        public static int RevisionVer = 1;
         public static int BuildVer = 0;
         public static int SCVersion = 1;
         public static int ValidatorIssueCount = 0;
